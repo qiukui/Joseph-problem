b90953c4e993ab1e4ef27cdd72bc74c402a88e33
qiukui <1051866112@qq.com>
2013-12-10 16:02:03 +0800

    update

--- main.cpp
+++ main.cpp
@@ -1,9 +1,9 @@
 #include<iostream>
+#include<assert.h>
 
 using namespace std;
 
-#define N 8
-#define M 4
+
 int joseph(int n,int m)
 {
 	if(n==1)
@@ -11,21 +11,31 @@ int joseph(int n,int m)
 	else
 		return (joseph(n-1,m)+m)%n;
 }
-void main()
+int joseph_iter(int n,int m)
 {
-	int a[N];
-	int n=N,k=N;
-	int i,p,num=0;
-	
-	cout<<joseph(8,4)<<endl;
-	for(i=0;i<N;i++)
+	int s=0;
+	for(int i=2;i<=n;i++)
+		s=(s+m)%i;
+	return s;
+}
+
+int joseph_simu(int n,int m)
+{
+    int *a = new int[n];
+	assert(a!=NULL);
+
+	int p=0;
+	int k=n;
+	int num=0;
+     
+	for(int i=0;i<n;i++)
 		a[i]=1;
-    p=0;
+   
 	while(k!=1)
 	{
 		if(a[p])
 		   num++;
-		if(num==M)
+		if(num==m)
 		{
 			num=0;
 			a[p]=0;
@@ -34,8 +44,15 @@ void main()
 		p=(p+1)%n;
 	}
 
-	for(i=0;i<N;i++)
-	 if(a[i]) cout<<i<<endl;
-	
+	for(i=0;i<n;i++)
+	 if(a[i]) return i;
+	return -1;
+}
+int main()
+{
+	cout<<joseph(8,4)<<endl;
+	cout<<joseph_iter(8,4)<<endl;
+    cout<<joseph_simu(8,4)<<endl;
+	return 0;
 }
 
